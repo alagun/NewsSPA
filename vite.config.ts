ca071281b7c26f1473926fa6e9379b8d6a7e1b29
Alexey Lagun <alexey.lagun@gmail.com>
2025-09-17 00:46:49 +0300

    feat: обновление зависимостей

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/YoutubeSPA/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/NewsSPA/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
