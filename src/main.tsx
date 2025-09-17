bda154258bf840523f64dee6528b4f64694a77a9
Alexey Lagun <alexey.lagun@gmail.com>
2025-09-17 11:56:10 +0300

    fix: исправлен корневой роутинг

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './app/store/store'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './app/App'
 import 'antd/dist/reset.css'
 import './app/styles/global.scss'
@@ -12,9 +12,9 @@ const root = createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
